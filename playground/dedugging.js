d8263533ab72b2a2c02e7c852fcf788bc383ad99
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-09-21 22:13:32 +0300

    Added file for demo debugger

---
+++ playground/dedugging.js
@@ -0,0 +1,17 @@
+var person = {
+  name: 'Guest'
+};
+
+person.age = 25;
+
+debugger;
+
+person.name = 'User';
+
+console.log(person);
+
+// for startb debug
+// tape: node inspect <file>
+// use n for next
+// use c for continue
+// use repl for debug vars
